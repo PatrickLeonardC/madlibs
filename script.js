df45d436f990166d636e8fb2e5cf574a99b53e3d
Patrick Leonard <lpatrick122307@gmail.com>
2024-08-08 20:01:07 +0000

    madlibs

---
+++ script.js
@@ -0,0 +1,29 @@
+// alert("welcome to my mad lib >:)")
+
+let name= "patrick"
+let is="is"
+let adjective="intellligent";
+let number=7;
+// calculated value
+let yuckymath= number * 7;
+let WillIBeRich= true;
+
+let string1="Patrick is intelligent."
+let string2= " The duck wanted to cross the road."
+let string3=" patrick helped the dog cross the road."
+
+let story1= string1 + string2 + string3
+console.log(story1)
+
+let string4="patrick does not like math."
+let string5=" the math teacher gives too much homework. :("
+let string6=" patrick uses javascript to cheat on his homework."
+let string7=" like this!"
+let string8=" 7*7 / 6 =?"
+let string9=" java helped him get..."
+let math1=7*7/6
+let emotion1=" patrick is happy now that he didnt have to do his homework! :smile:"
+
+
+let story2= string4 + string5 + string6 +string7 + string8 + string9 + math1 + emotion1
+console.log(story2)
